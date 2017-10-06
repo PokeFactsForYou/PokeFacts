5e0f29b6418dca356d6deacf4d1155024928e21c
DinoTroll <jmh10e@my.fsu.edu>
2017-10-06 08:37:02 -0400

    We have facts..About Pokemon

---
+++ PokeFacts.rb
@@ -0,0 +1,4 @@
+pokeFacts = ["If you reject all three starting Pokemon in Pokemon Go by walking away from them, you're given the chance to catch Pikachu.",
+			"There is a whole species of Pterosaur named after the Pokemon Aerodactyl, called Aerodactylus Scolopaciceps.", 
+			"There's only one save file in the original Pokemon games because the developers wanted you to be able to name all of your 151 Pokemon. There wasn't room to name them all on multiple profiles."]
+
